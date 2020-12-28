8e57427e840b9f96764ab21c3a0ced09d5c1bac7
Daniel Blagy <daniel.blagy@gmail.com>
2020-12-28 16:22:03 +0300

    added an algorithm to build a node tree of file system

--- wm3dfsv.js
+++ wm3dfsv.js
@@ -5,7 +5,8 @@ const NodeType = {
 Object.freeze(NodeType);
 
 class Node {
-	constructor(root, children, type) {
+	constructor(name, root, children, type) {
+		this.name = name;
 		this.root = root;
 		this.children = children;
 		this.type = type;
@@ -17,16 +18,63 @@ function init()
 	var input = document.getElementById("file-picker");
 	
 	input.onchange = e => { 
-	   var files = e.target.files;
-	   console.log(files);
-	   input.remove();
-	   start(files);
+	   	var files = e.target.files;
+	   	console.log(files);
+	   	input.remove();
+	   	start(files);
 	}
+}
+
+function create_node(current_node, dirs, count, length) {
+	let found = false;
+	
+	var i;
+	for (i = 0; i < current_node.children.length; i++) {
+		if (current_node.children[i].name === dirs[count]) {
+			found = true;
+			break;
+		}
+	}
+	
+	if (found)
+		current_node = current_node.children[i];
+	else {
+		// if name includes '.', then we'll consider it a file, otherwise it's a directory
+		if (dirs[count].includes("."))
+			current_node.children.push(new Node(dirs[count], current_node, [], NodeType.File));
+		else
+			current_node.children.push(new Node(dirs[count], current_node, [], NodeType.Directory));
+		
+		current_node = current_node.children[current_node.children.length - 1];
+	}
+	
+	count++;
+	
+	if (count < length)
+		create_node(current_node, dirs, count, length);
+}
+
+function display_node_tree(root) {
+	console.log(root);
 }
 
 function start(files) {
 	// separate root dir name from the files in array
 	// for each file in array determine its position in the file system tree and create a node object
+	
+	var root_node = new Node(files[0]["webkitRelativePath"].split("/")[0], undefined, [], NodeType.Directory);
+	
+	// brute force algorithm
+	for (const file of files) {
+		let dirs = file["webkitRelativePath"].split("/");
+		dirs.shift();	// remove the first element (which is a name of the root dir)
+		create_node(root_node, dirs, 0, dirs.length);
+		/*dirs.forEach(function(element) {
+			create_node(root_node, element);
+		});*/
+	}
+	
+	console.log(root_node);
 	
 	const scene = new THREE.Scene();
 	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
