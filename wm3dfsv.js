8f5d317eb741eeeba5e15c654840b584eb222840
Daniel Blagy <daniel.blagy@gmail.com>
2021-01-02 17:27:58 +0300

    working on labels

--- wm3dfsv.js
+++ wm3dfsv.js
@@ -54,17 +54,30 @@ function create_node(current_node, dirs, count, length) {
 		create_node(current_node, dirs, count, length);
 }
 
-function make_geometry(scene, node, i) {
-	console.log("make_geometry call ", i);
+function make_geometry(scene, node, x, z) {
+	console.log("make_geometry call x: ", x, "  z: ", z);
 	const geometry = new THREE.BoxGeometry(5, 5, 5);
 	const material = new THREE.MeshBasicMaterial( { color: 0x15384a } );
+	//const material = new THREE.MeshBasicMaterial( { color: () } );
 	const cube = new THREE.Mesh(geometry, material);
-	cube.position.x = 10 * i;
-	//cube.position.y = 50 * i;
+	cube.position.x = x;
+	cube.position.z = z;
 	scene.add(cube);
 	
+	let label = new THREE.TextSprite({
+		text: node.name,
+		fontFamily: 'Arial, Helvetica, sans-serif',
+		fontSize: 4,
+		color: '#ffbbff',
+	  });
+	cube.add(label);
+	
+	let i = 0;
+	let sign = 1;
 	for (const child of node.children) {
-		make_geometry(scene, child, ++i);
+		make_geometry(scene, child, sign * 10 * i, z - 10);
+		i++;
+		sign *= -1;
 	}
 }
 
@@ -88,6 +101,7 @@ function start(files) {
 	
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0x4B5A20, 1.0);
 	document.body.appendChild(renderer.domElement);
 	
 	//const geometry = new THREE.BoxGeometry();
@@ -105,16 +119,33 @@ function start(files) {
 	//
 	//scene.add(group);
 	
-	make_geometry(scene, root_node, 0);
+	let sprite = new THREE.TextSprite({
+		text: 'Hello World!',
+		fontFamily: 'Arial, Helvetica, sans-serif',
+		fontSize: 12,
+		color: '#ffbbff',
+	  });
+	scene.add(sprite);
+	
+	const controls = new THREE.OrbitControls(camera, renderer.domElement);
+	// controls.update() must be called after any manual changes to the camera's transform
+	//controls.update();
+	
+	make_geometry(scene, root_node, 0, 0);
 			
 	camera.position.z = 50;
 	
 	function update_and_render() {
 		requestAnimationFrame(update_and_render);
+		
 		//group.rotation.x += 0.01;
 		//group.rotation.y += 0.035;
 		//group.rotation.z -= 0.015;
 		//group.position.z += 0.01;
+		
+		// required if controls.enableDamping or controls.autoRotate are set to true
+		controls.update();
+		
 		renderer.render(scene, camera);
 	}
 	
