7c8a79c1bb35c9b8d9097b5d33458bb29734405c
Daniel Blagy <daniel.blagy@gmail.com>
2021-01-05 20:53:59 +0300

    turned on antialias for the renderer

--- wm3dfsv.js
+++ wm3dfsv.js
@@ -129,7 +129,7 @@ function start(files) {
 	const scene = new THREE.Scene();
 	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	
-	const renderer = new THREE.WebGLRenderer();
+	const renderer = new THREE.WebGLRenderer({antialias : true});
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.setClearColor(0x4B5A20, 1.0);
 	document.body.appendChild(renderer.domElement);
